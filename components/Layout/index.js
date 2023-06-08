8ed70740010f72bc4bde7a6795cfee1c18e174c5
Dwi Herydo Gultom <edogultom10395@gmail.com>
2023-06-08 23:40:17 +0700

    update: perbaikan

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       <Navbar />
       {/* <main className=" mt-[47px] md:mt-[70px] py-12 px-2 md:py-[48px] lg:py-[48px] lg:px-4 md:max-w-3xl xl:max-w-full lg:max-w-5xl"> */}
       <div className="container mx-auto">
-        <main className=" mt-[57px] md:mt-[70px] md:py-[48px] lg:py-[48px] lg:px-4 w-full ">
+        <main className=" mt-[57px] md:mt-[70px] md:py-[48px] lg:py-[25px] lg:px-4 w-full ">
           {children}
         </main>
       </div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -20,31 +20,31 @@ const Projects = ({ response }) => {
   const data = JSON.parse(response);
   return (
     <>
-      <div className="bg-red-200 w-full">
+      <div className="w-full">
         <Head>
           <title>{data.name}</title>
         </Head>
         <Layout idTab={data.id}>
-          {/* <div className="relative flex flex-col min-w-0 break-words w-full mb-6 mt-4 ">
-          <div className="px-4 py-2 flex-auto">
-            <div className="tab-content tab-space">
-              <div className="block">
-                <div className=" flex flex-col lg:flex-col lg:gap-x-3 gap-y-3  rounded-lg">
-                  <div className="w-full">
-                    {(data.id.toString() === "1" ||
-                      data.id.toString() === "2") && (
-                      <WebSlider data={data.projectsList} />
-                    )}
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 mt-4 ">
+            <div className="px-4 py-2 flex-auto">
+              <div className="tab-content tab-space">
+                <div className="block">
+                  <div className=" flex flex-col lg:flex-col lg:gap-x-3 gap-y-3  rounded-lg">
+                    <div className="w-full">
+                      {(data.id.toString() === "1" ||
+                        data.id.toString() === "2") && (
+                        <WebSlider data={data.projectsList} />
+                      )}
 
-                    {data.id.toString() === "3" && (
-                      <MobileSlider data={data.projectsList} />
-                    )}
+                      {data.id.toString() === "3" && (
+                        <MobileSlider data={data.projectsList} />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div> */}
         </Layout>
       </div>
     </>
